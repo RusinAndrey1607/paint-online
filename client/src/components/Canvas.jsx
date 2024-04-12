6b8aacb65ba15380b88b48f9c4cad45eabc7e9d3
RusinAndrey1607 <66871844+RusinAndrey1607@users.noreply.github.com>
2024-04-12 10:00:48 +0300

    Setup client app

---
+++ client/src/components/Canvas.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "../styles/canvas.scss"
+
+export default function Canvas() {
+  return (
+    <div className='canvas'>Canvas</div>
+  )
+}
---
+++ client/src/components/SettingBar.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "../styles/toolbar.scss"
+
+export default function SettingBar() {
+  return (
+    <div className='setting-bar'>SettingBar</div>
+  )
+}
