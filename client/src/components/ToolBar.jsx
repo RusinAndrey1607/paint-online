3e3c45f08169bb890466e6c8b4bc3ac19df11a45
RusinAndrey1607 <66871844+RusinAndrey1607@users.noreply.github.com>
2024-04-14 17:52:49 +0300

    Add brush and Rect tools

--- client/src/components/ToolBar.jsx
+++ client/src/components/ToolBar.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import "../styles/toolbar.scss"
+import canvasState from '../store/canvasState'
+import toolState from '../store/toolState'
+import Brush from '../tools/Brush'
+import Rect from '../tools/Rect'
 
 export default function ToolBar() {
+  
   return (
     <div className='toolbar'>
-        <button className="toolbar__btn brush"></button>
-        <button className="toolbar__btn rect"></button>
-        <button className="toolbar__btn circle"></button>
-        <button className="toolbar__btn eraser"></button>
-        <button className="toolbar__btn line"></button>
+        <button className="toolbar__btn brush" onClick={() => toolState.setTool(new Brush(canvasState.canvas))}></button>
+        <button className="toolbar__btn rect" onClick={() => toolState.setTool(new Rect(canvasState.canvas))}></button>
+        <button className="toolbar__btn circle" onClick={() => toolState.setTool(new Brush(canvasState.canvas))}></button>
+        <button className="toolbar__btn eraser" onClick={() => toolState.setTool(new Brush(canvasState.canvas))}></button>
+        <button className="toolbar__btn line" onClick={() => toolState.setTool(new Brush(canvasState.canvas))}></button>
         <input type="color" className='toolbar__input'/>
         <button className="toolbar__btn undo"></button>
         <button className="toolbar__btn redo"></button>
