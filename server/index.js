07951fc00d3553c4ca4d14bbd9a6d108e90a063e
RusinAndrey1607 <66871844+RusinAndrey1607@users.noreply.github.com>
2024-04-18 08:36:14 +0300

    Add connection handler

--- server/index.js
+++ server/index.js
@@ -1,12 +1,30 @@
-const express = require('express')
-const app = express()
-const WSSserver = require('express-ws')(app)
+const express = require("express");
+const app = express();
+const WSSserver = require("express-ws")(app);
+const aWss = WSSserver.getWss()
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000
-
-app.ws("/",(ws,req) =>{
-    console.log("Connection");
+app.ws("/", (ws, req) => {
+  ws.on("message", (msg) => {
+    msg = JSON.parse(msg);
+    switch (msg.method) {
+      case "connection":
+        connectionHandler(ws, msg);
+        break
+    }
+  });
+});
+app.listen(PORT, () => {
+  console.log("Server started on PORT ", PORT);
+});
+const connectionHandler = (ws, msg) => {
+    ws.id = msg.id
+    broadcastConnection(ws,msg)
+};
+const broadcastConnection = (ws,msg) =>{
+    aWss.clients.forEach(client => {
+        if(client.id === msg.id){
+            client.send(`User ${msg.username} connected`)
+        }
 })
-app.listen(PORT,() =>{
-    console.log("Server started on PORT ", PORT);
-})
+}
